664179a40a36e2808f43ea495be993026ba55c9e
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2024-08-02 10:57:09 -0700

    [743] [Time Beats: 30.88%] [Memory Beats: 34.31%] - LeetPush

--- 743-Network-Delay-Time.py
+++ 743-Network-Delay-Time.py
@@ -1,25 +1,22 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj = {i:[] for i in range(n)}
+        adj = {i:[] for i in range(1, n+1)}
 
         for source, target, weight in times:
-            adj[source-1].append((target-1, weight))
-
-        distances = [float("inf")] * n
-        distances[k-1] = 0
+            adj[source].append((target, weight))
+        
+        seen = set()
+        minHeap = [(0,k)]
         ans = 0
-        minHeap = [(0, k-1)]
-
         while minHeap:
             weight, node = heapq.heappop(minHeap)
-            if weight > distances[node]:
+            if node in seen:
                 continue
+            seen.add(node)
+            ans = max(weight, ans)
             for neighbor, neighborWeight in adj[node]:
-                dist = weight + neighborWeight
-                if dist < distances[neighbor]:
-                    distances[neighbor] = dist
-                    heapq.heappush(minHeap, (dist, neighbor))
-        
-        ans = max(distances)
-        return ans if ans < float('inf') else -1
+                if neighbor not in seen:
+                    heapq.heappush(minHeap, (neighborWeight + weight, neighbor)) 
+        return ans if len(seen) == n else -1
+
         
