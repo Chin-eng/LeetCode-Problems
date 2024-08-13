f28f04ef166fc44d38a632a26ed8091f0e0e9e45
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2024-08-12 21:59:00 -0700

    [70] [Time Beats: 5.92%] [Memory Beats: 56.44%] - LeetPush

--- 70-Climbing-Stairs.py
+++ 70-Climbing-Stairs.py
@@ -1,16 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
+        @cache
         def dp(i):
-            if i in memo:
-                return memo[i]
             if i == 1:
                 return 1
             if i == 2:
                 return 2
-            memo[i] = dp(i-1) + dp(i-2)
-            return memo[i]
+            return dp(i-1) + dp(i-2)
         return dp(n)
+
 
 
         
