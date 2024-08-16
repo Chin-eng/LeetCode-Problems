726baa156102b2dd53149768d7d7ac31fa326421
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2024-08-15 19:07:56 -0700

    [1456] [Time Beats: 19.17%] [Memory Beats: 71.89%] - LeetPush

--- 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.py
+++ 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        vowel ={'a', 'e', 'i', 'o', 'u'}
-        left, count, res = 0,0,0
+        vowels = {"a", 'e', 'i', 'o', 'u'}
+
+        left, result, count = 0, 0, 0
 
         for right in range(len(s)):
-            count += 1 if s[right] in vowel else 0
+            count += 1 if s[right] in vowels else 0
             if right - left + 1 > k:
-                count -= 1 if s[left] in vowel else 0
-                left += 1
-            res = max(count, res)
-        return res
+                count -= 1 if s[left] in vowels else 0
+                left += 1 
+            result = max(count, result)
+        return result
+
+        
+        
