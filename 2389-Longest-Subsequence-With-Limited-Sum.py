61ba202a04f48e4e6a29a997dc24c07f3795374a
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2024-08-26 18:30:29 -0700

    [2389] [Time Beats: 90.02%] [Memory Beats: 70.29%] - LeetPush

--- 2389-Longest-Subsequence-With-Limited-Sum.py
+++ 2389-Longest-Subsequence-With-Limited-Sum.py
@@ -1,22 +1,13 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        result = []
-        nums = sorted(nums)
-        for number in queries:
-            current_sum = 0
-            count = 0
-            index = 0
-            while index < len(nums):
-                current_sum += nums[index]
-                if number >= current_sum:
-                    count += 1
-                else:
-                    current_sum -= nums[index]
-                index += 1
-            result.append(count)
-        return result
-
-
+        nums.sort()
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
         
-        
+        ans = []
+
+        for query in queries:
+            index = bisect_right(nums, query)
+            ans.append(index)
+        return ans
         
