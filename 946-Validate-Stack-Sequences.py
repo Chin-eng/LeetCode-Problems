0f281e89c487d31c5c718728eee4c9f6fe1c94c4
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2024-09-03 20:00:58 -0700

    [946] [Time Beats: 69.19%] [Memory Beats: 39.56%] - LeetPush

---
+++ 946-Validate-Stack-Sequences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        i = 0
+        for n in pushed:
+            stack.append(n)
+            while stack and popped[i] == stack[-1]:
+                stack.pop()
+                i += 1
+        return not stack
+        
