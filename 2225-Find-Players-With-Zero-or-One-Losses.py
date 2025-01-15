3bebf8b13fc6ae86aaf321175f8c0897052cc0b7
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-01-15 06:05:49 -0800

    [2225] [Time Beats: 25.81%] [Memory Beats: 37.61%] - LeetPush

---
+++ 2225-Find-Players-With-Zero-or-One-Losses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+
+        winnerHash = defaultdict(int)
+        loserHash = defaultdict(int)
+
+        notLost = []
+        oneLoss = []
+
+        for winner, loser in matches:
+            winnerHash[winner] += 1
+            loserHash[loser] += 1
+        
+        for key in winnerHash:
+            if key not in loserHash:
+                notLost.append(key)
+        
+        for key in loserHash:
+            if loserHash[key] == 1:
+                oneLoss.append(key)
+
+        notLost.sort()
+        oneLoss.sort()
+
+        ans = []
+
+        ans.append(notLost)
+        ans.append(oneLoss)
+
+        return ans
+        
