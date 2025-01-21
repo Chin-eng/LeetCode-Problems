3f9ff06e6c22334e324758c1d582b392887540fa
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-01-20 21:20:30 -0800

    [2260] [Time Beats: 15.58%] [Memory Beats: 6.89%] - LeetPush

--- 2260-Minimum-Consecutive-Cards-to-Pick-Up.py
+++ 2260-Minimum-Consecutive-Cards-to-Pick-Up.py
@@ -1,14 +1,18 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        hashmap = defaultdict(int)
-        # print(cards.index(8))
+        hashmap = defaultdict(list)
+        for i in range(len(cards)):
+            hashmap[cards[i]].append(i)
+
+        hashmap_values = hashmap.values()
+        
         ans = float("inf")
 
-        for index, card in enumerate(cards):
-            if card in hashmap:
-                ans = min(ans, index - hashmap[card] + 1)
-            hashmap[card] = index
-        
+        for value in hashmap_values:
+            array = value
+            for i in range(1, len(array)):
+                ans = min(ans, array[i] - array[i-1] + 1)
+
         return ans if ans != float("inf") else -1
-        # return -1
-                
+
+        
