d43a55a0e5373e967699c685b5e3b12c25d53a97
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-01-30 03:37:18 -0800

    [1512] [Time Beats: 100.00%] [Memory Beats: 73.12%] - LeetPush

--- 1512-Number-of-Good-Pairs.py
+++ 1512-Number-of-Good-Pairs.py
@@ -1,19 +1,11 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         hashmap = defaultdict(int)
-
-        def formula(n):
-            return n * (n - 1) // 2
-
+        ans = 0
         for num in nums:
+            if num in hashmap:
+                ans += hashmap[num]
             hashmap[num] += 1
-        
-        ans  = 0
-
-        for value in hashmap.values():
-            pairs = formula(value)
-            ans += pairs
 
         return ans
-
         
