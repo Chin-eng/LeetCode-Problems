eacc1d1253f5e35a8d27323ed352481de4e545a3
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-02-01 04:25:53 -0800

    [567] [Time Beats: 42.10%] [Memory Beats: 15.04%] - LeetPush

--- 567-Permutation-in-String.py
+++ 567-Permutation-in-String.py
@@ -1,7 +1,7 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
-        hashmap1 = defaultdict(int)
+        s2_hashmap = defaultdict(int)
         s1_hashmap = Counter(s1)
 
         def anagram(s1_hashmap, hashmap1):
@@ -13,14 +13,14 @@ class Solution:
         left = 0
         for right in range(len(s2)):
             char_right = s2[right]
-            hashmap1[char_right] += 1
+            s2_hashmap[char_right] += 1
             while right - left + 1 > len(s1):
                 char_left = s2[left]
-                hashmap1[char_left] -= 1
-                if hashmap1[char_left] == 0:
-                    hashmap1.pop(char_left)
+                s2_hashmap[char_left] -= 1
+                if s2_hashmap[char_left] == 0:
+                    s2_hashmap.pop(char_left)
                 left += 1
-            if len(s1_hashmap) == len(hashmap1) and anagram(s1_hashmap, hashmap1):
+            if len(s1_hashmap) == len(s2_hashmap) and anagram(s1_hashmap, s2_hashmap):
                 return True
         
         return False
