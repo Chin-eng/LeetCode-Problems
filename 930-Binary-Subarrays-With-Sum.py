1e6faf28d9e609505bab6d493fcc67ecd77ed38c
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-02-05 06:56:40 -0800

    [930] [Time Beats: 37.81%] [Memory Beats: 6.42%] - LeetPush

--- 930-Binary-Subarrays-With-Sum.py
+++ 930-Binary-Subarrays-With-Sum.py
@@ -1,19 +1,19 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        prefix = [nums[0]]
 
-        def helper(goal):
-            if goal < 0: return 0
-            left2, ans2, curr2 = 0, 0, 0
-            for right in range(len(nums)):
-                curr2 += nums[right]
-                while left2 <= right and curr2 > (goal):
-                    curr2 -= nums[left2]
-                    left2 += 1
-                ans2 += right - left2 + 1
-            return ans2
-        
-        return helper(goal) - helper(goal -1)
-        
+        for i in range(1, len(nums)):
+            prefix.append(nums[i] + prefix[-1])
+
+        hashmap = defaultdict(int)
+        hashmap[0] = 1
+
+        ans = 0
+        for num in prefix:
+            ans += hashmap[num-goal]
+            hashmap[num] += 1
+
+        return ans
         
         
         
