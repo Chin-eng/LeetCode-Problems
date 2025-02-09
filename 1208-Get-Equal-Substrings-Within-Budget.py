4f9ae60ab7ce274556fb6c4bccb2bbbfaf5dd976
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-02-09 05:18:34 -0800

    [1208] [Time Beats: 35.66%] [Memory Beats: 59.26%] - LeetPush

--- 1208-Get-Equal-Substrings-Within-Budget.py
+++ 1208-Get-Equal-Substrings-Within-Budget.py
@@ -1,17 +1,18 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
 
-        result, left, current_cost = 0, 0, 0
-        for right in range(len(s)):
-            current_cost += abs(ord(s[right]) - ord(t[right]))
-            while current_cost > maxCost:
-                current_cost -= abs(ord(s[left]) - ord(t[left]))
-                left += 1
-            result = max(result, right - left + 1)
-        return result
-       
-       
-       
+        if len(s) != len(t):
+            return 0
 
+        ans, left, cost = float("-inf"), 0, 0
+
+        for right in range(len(s)):
+            cost += abs(ord(s[right]) - ord(t[right]))
+            while left <= right and cost > maxCost:
+                cost -= abs(ord(s[left]) - ord(t[left]))
+                left += 1
+            ans = max(ans, right - left + 1)
+
+        return ans
 
         
