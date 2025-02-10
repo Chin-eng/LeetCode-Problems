60e807e2abd859e6faebbd3e753742a398c48284
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-02-10 06:48:04 -0800

    [424] [Time Beats: 32.08%] [Memory Beats: 94.27%] - LeetPush

--- 424-Longest-Repeating-Character-Replacement.py
+++ 424-Longest-Repeating-Character-Replacement.py
@@ -2,20 +2,12 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
         hashmap = defaultdict(int)
-
-        def getMax(hashmap):
-            maxVal = float('-inf')
-            for value in hashmap.values():
-                maxVal = max(maxVal, value)
-            return maxVal
-
         
         left , ans = 0, float('-inf')
         for right in range(len(s)):
             hashmap[s[right]] += 1
-            maxOccurance = getMax(hashmap)
 
-            while (right - left + 1) - maxOccurance > k:
+            while (right - left + 1) - max(hashmap.values()) > k:
                 hashmap[s[left]] -= 1
                 left += 1                     
             
