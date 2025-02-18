67a1a86388a3385a0e372c2b09b88a32e4cd9be3
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-02-18 23:18:42 +0800

    [76] [Time Beats: 75.72%] [Memory Beats: 25.28%] - LeetPush

--- 76-Minimum-Window-Substring.py
+++ 76-Minimum-Window-Substring.py
@@ -5,38 +5,34 @@ class Solution(object):
         :type t: str
         :rtype: str
         \\\
-
-        if len(s) < len(t): return \\
-        if t == \\: return \\
-
-        Have = 0
-
+        Have, Need = 0, 0
         hashmap_t = defaultdict(int)
-        for string in t:
-            hashmap_t[string] += 1
-        
+        for letter in t:
+            hashmap_t[letter] += 1
+            
         Need = len(hashmap_t)
-        
+        left, start, end, minSize = 0, 0, 0, float(\inf\)
         hashmap_s = defaultdict(int)
-
-        left ,start, end, minSize = 0, 0, 0, float(\inf\)
         
         for right in range(len(s)):
             if s[right] in hashmap_t:
                 hashmap_s[s[right]] += 1
-            
             if s[right] in hashmap_t and hashmap_s[s[right]] == hashmap_t[s[right]]:
                 Have += 1
-            
+
             while Have == Need:
-                if (right - left + 1) < minSize:
+                if right - left + 1 < minSize:
                     start, end = left, right
                     minSize = right - left + 1
-                if s[left] in hashmap_t:
-                    hashmap_s[s[left]] -= 1
-                if s[left] in hashmap_t and hashmap_s[s[left]] < hashmap_t[s[left]] :
+
+                hashmap_s[s[left]] -= 1 
+                
+                if s[left] in hashmap_t and hashmap_s[s[left]] < hashmap_t[s[left]]:
                     Have -= 1
+
                 left += 1
-        
+
+     
         return s[start:end+1] if minSize != float(\inf\) else \\
+                
         
